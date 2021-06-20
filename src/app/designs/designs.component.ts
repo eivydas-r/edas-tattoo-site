8c103a1eee1a3a3b29e18fcfd9f728e71bedc52c
eivydas-r <eivydas.cs@gmail.com>
2021-06-19 22:16:51 -0500

    visual updates

--- src/app/designs/designs.component.ts
+++ src/app/designs/designs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-designs',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesignsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) {
+    titleService.setTitle("Edas Tattoo: Designs");
+  }
 
   ngOnInit(): void {
   }
--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-location',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) {
+    titleService.setTitle("Edas Tattoo: Location");
+  }
 
   ngOnInit(): void {
   }
